3e312d22f20626d843457021799808feb108f223
Vibhav-Jaiswal <vibhavjaiswal9793@gmail.com>
2023-10-24 10:51:45 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const inputBox = document.getElementById('input-box')
+const button = document.getElementById('btn')
+const listContainer = document.getElementById('list-container')
+
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert('You must write something!')
+    }
+    else{
+        let li = document.createElement("li")
+        li.textContent = inputBox.value;
+        listContainer.append(li);
+
+        let span = document.createElement('span');
+        span.textContent = '\u00d7';
+        li.append(span);
+    }
+    inputBox.value = "";   
+    saveData();
+}
+
+
+function handleList(e) {
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+    saveData();
+}
+
+
+function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML);
+}
+
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+
+button.addEventListener('click', addTask)
+
+listContainer.addEventListener('click', handleList)
+
+showTask();
